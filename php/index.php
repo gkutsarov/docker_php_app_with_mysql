9d8c33ff6bff5043c6c6fc5b506209f0c2bece1c
grolfskin <gkutsarov123@gmail.com>
2024-10-23 16:29:01 +0300

    Initial commit.

---
+++ php/index.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "mysql";
+$username = "user";
+$password = "password";
+$dbname = "myapp";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Simple query to fetch data
+$sql = "SELECT id, name FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+
